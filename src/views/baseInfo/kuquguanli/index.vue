5fd19f08ae85fdad88a005d6b59a404c7c5517e1
lxx0303 <2434921421@qq.com>
2022-07-28 20:58:08 +0800

    库区管理初始化，建错了分支

---
+++ src/views/baseInfo/kuquguanli/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div>
+      <Header></Header>
+    </div>
+    <!-- 搜索部分 -->
+    <div>
+      <el-card>
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="grid-content">
+              <div class="text">仓库编号</div>
+              <el-input v-model="inputNumber" placeholder="请输入"> </el-input>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content">
+              <div class="text">仓库名称</div>
+              <el-input v-model="inputName" placeholder="请输入"> </el-input>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content">
+              <div class="text">仓库状态</div>
+              <el-select
+                v-model="currentStuts"
+                placeholder="请输入"
+                @change="changeClick"
+              >
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                >
+                </el-option>
+              </el-select>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content fcc btns">
+              <el-button type="warning" round @click="onWarehoseInfo"
+                >搜索</el-button
+              >
+              <el-button round @click="resetClick">重置</el-button>
+            </div>
+          </el-col>
+        </el-row>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/compoents/Header.vue";
+export default {
+  components: { Header },
+};
+</script>
+
+<style scoped lang="less">
+// 文字
+.text {
+  font-size: 12px;
+  color: #887e7e;
+  margin-bottom: 10px;
+}
+</style>
